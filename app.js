af3c0a13b33102a251b7b160b1c73e3f12bec607
mrgcl <maramirezgutierrez@gmail.com>
2016-03-02 05:08:59 -0300

    heroku port

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.get('/about', function(req, res){
     res.send('About Us.');
 });
 
 app.use(express.static(__dirname + '/public'));
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log('Express server started.');
 });
