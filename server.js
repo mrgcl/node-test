8ff31e6bb39832412b8d81fdb475a30d9a079cc7
mrgcl <maramirezgutierrez@gmail.com>
2016-03-04 01:55:07 -0300

    post for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'whatever d00d',
-    completed: false
-},{
-    id: 2,
-    description: 'finish lesson',
-    completed: false
-},{
-    id: 3,
-    description: 'done',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('Todo API Root.');
@@ -25,10 +18,7 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var id = parseInt(req.params.id, 10);
-    var matched;
-    todos.forEach(function(todo){
-        if(todo.id === id) matched = todo;
-    });
+    var matched = _.findWhere(todos, {id: id});
     if(matched){
         res.json(matched);
     }
@@ -37,6 +27,15 @@ app.get('/todos/:id', function(req, res){
     }
 });
 
+app.post('/todos', function(req, res){
+    var body = req.body;
+    if(!_.isBoolean(body.completed)  || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+    body.id = todoNextId++;
+    todos.push(body);
+    res.json(body);
+});
 
 app.listen(PORT, function(){
     console.log('Express server started.');
