9860df1cd91b683a5424aede3b2072479c35b669
mrgcl <maramirezgutierrez@gmail.com>
2016-03-15 05:46:13 -0300

    local todo app ready

--- server.js
+++ server.js
@@ -13,7 +13,20 @@ app.get('/', function(req, res){
 });
 
 app.get('/todos', function(req, res){
-   res.json(todos);
+    var params = req.query;
+    var filteredTodos = todos;
+    if(params.hasOwnProperty('completed') && params.completed === 'true'){
+        filteredTodos = _.where(filteredTodos, {"completed" : true});
+    }
+    else if(params.hasOwnProperty('completed') && params.completed === 'false'){
+        filteredTodos = _.where(filteredTodos, {"completed" : false});
+    }
+    if(params.hasOwnProperty('q') && params.q.trim().length > 0){
+        filteredTodos = _.filter(filteredTodos, function(todo){
+            return todo.description.toLowerCase().indexOf(params.q.toLowerCase()) > -1;
+        });
+    }
+    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req, res){
@@ -36,6 +49,42 @@ app.post('/todos', function(req, res){
     body.id = todoNextId++;
     todos.push(body);
     res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res){
+    var id = parseInt(req.params.id, 10);
+    var matched = _.findWhere(todos, {id: id});
+    if(!matched){
+        res.status(404).json({"error": "no todo found with that id"});
+    }
+    else{
+        todos = _.without(todos, matched);
+        res.json(matched);
+    }
+});
+
+app.put('/todos/:id', function(req, res){
+    var id = parseInt(req.params.id, 10);
+    var matched = _.findWhere(todos, {id: id});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    if(!matched){
+        return res.status(404).send();
+    }
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }
+    else if (body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }
+    else if (body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+    _.extend(matched, validAttributes);
+    res.json(matched);
 });
 
 app.listen(PORT, function(){
